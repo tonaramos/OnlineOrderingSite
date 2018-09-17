818a12baaf0ba0e5e8e32087d8c24bb34e299448
Tona Ramos <gtonaramos@gmail.com>
2018-09-16 21:11:21 -0700

    Create react app ejected and project folders added.

---
+++ src/containers/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import Layout from '../components/Layout/Layout';
+import Builder from './Builder/Builder';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Layout>
+        <div className="pageContents">
+          <Builder />
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default App;
