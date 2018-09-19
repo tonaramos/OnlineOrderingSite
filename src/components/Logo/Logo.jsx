70069bcccc4c5274315efbc364be8f556aa89380
Tona Ramos <gtonaramos@gmail.com>
2018-09-18 21:18:14 -0700

    Added SideDrawe for mobil devices.

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css';
 
-const logo = (props) => (
-    <div className={classes.Logo}>
-      <img className={classes.Logo} src={burgerLogo} alt='MyBurger'  />
+const logo = props => {
+  const { height } = props;
+  return(
+    <div className={classes.Logo} style={{height: height}}>
+      <img src={burgerLogo} alt='MyBurger' />
     </div>
-);
+  )
+};
 
 export default logo;
--- src/components/Navigation/Navigation/NavigationItem/NavigationItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const navigationItem = (props) => (
-  <li>
-    <a herf="/">A link</a>
-  </li>
-);
-
-export default navigationItem;
